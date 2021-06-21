7a8d9e9627c26ada2bdfa342d2bbbde823bc0c2e
Piotr Przybys <piotrek.przybys@gmail.com>
2021-06-22 01:02:36 +0200

    Homework 3: pointers, slice, maps

--- module-3/task2/slices/slice.go
+++ module-3/task2/slices/slice.go
@@ -1,10 +1,16 @@
 package slices
 
+import "sort"
+
 func SortStringsInPlace(s []string) {
+	sort.Strings(s)
 }
 
 func SortStringsPure(s []string) []string {
-
+	var s2 = make([]string, len(s))
+	copy(s2, s)
+	sort.Strings(s2)
+	return s2
 }
 
 type User struct {
@@ -13,9 +19,25 @@ type User struct {
 }
 
 func SortUsersPure(s []User) []User {
-	return r
+	u := make([]User, len(s))
+	copy(u, s)
+	sort.Slice(u, func(i, j int) bool {
+		if u[i].FirstName != u[j].FirstName {
+			return u[i].FirstName < u[j].FirstName
+		}
+		return u[i].LastName < u[j].LastName
+	})
+	return u
 }
 
 func SortUsersPureDesc(s []User) []User {
-	return r
+	u := make([]User, len(s))
+	copy(u, s)
+	sort.Slice(u, func(i, j int) bool {
+		if u[i].FirstName != u[j].FirstName {
+			return u[i].FirstName > u[j].FirstName
+		}
+		return u[i].LastName > u[j].LastName
+	})
+	return u
 }
